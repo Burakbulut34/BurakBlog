05a9b55aa291b253985d08e0d77350e4b5f9ef22
Kloxyn <126696632+Burakbulut34@users.noreply.github.com>
2025-10-05 09:54:58 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./footer.css";
 
 export default function Footer() {
+
+  const goToPage = ()=>{
+    window.location.href = "https://github.com/Burakbulut34"
+  };
   return (
     <footer className="footer">
       <hr className="footer-divider" />
       <div className="footer-content">
         <div className="footer-left">
-          <span className="name">
+          <span className="name" onclick={goToPage}>
             Burak <span className="highlight">Bulut</span>
           </span>
         </div>
